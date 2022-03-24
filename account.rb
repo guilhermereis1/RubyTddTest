2a838128c094b35380f6acb79c6d2ad1fbc124cd
Guilherme R <guilhermejosereis27@gmail.com>
2022-03-24 15:30:46 -0400

    Fineshed tests

--- account.rb
+++ account.rb
@@ -1,9 +1,11 @@
 class Account
   attr_reader :balance
 
-  def initialize(balance = 0.0, tax_per_withdraw = 0.1)
+  def initialize(balance = 0.0, tax_per_withdraw = 0.1, deposit_profit = 0.05, deposit_profit_limit = 50.0)
     @balance = balance
     @tax_per_withdraw = tax_per_withdraw
+    @deposit_profit = deposit_profit
+    @deposit_profit_limit = deposit_profit_limit
   end
 
   def show_balance
@@ -11,10 +13,19 @@ class Account
   end
 
   def deposit(amount)
-    amount > 50 ? @balance += amount + amount * 0.05 : @balance += amount
+    if amount > 0 then
+      amount > @deposit_profit_limit ? @balance += amount + amount * @deposit_profit : @balance += amount
+    else
+      'Invalid amount'
+    end
   end
 
   def withdraw(amount)
-    @balance -= amount + amount * @tax_per_withdraw
+    if amount > 0 then
+      @balance -= amount + amount * @tax_per_withdraw
+      @balance = 0 if @balance < amount
+    else
+      'Value must be greater than zero'
+    end
   end
 end
--- account_rspec.rb
+++ account_rspec.rb
@@ -28,8 +28,11 @@ describe Account do
   end
   
   context '#account has tax to withdraw' do
-    it 'Each withdraw should have a tax 10%' do
+    before do
       @account = Account.new
+    end
+
+    it 'Each withdraw should have a tax 10%' do
       @account.deposit(100.0)
       @account.withdraw(10.0)
       expect(@account.balance).to eq(94.0)
@@ -37,11 +40,37 @@ describe Account do
   end
 
   context '#account has a profit of 5% each deposit above 50' do
-    it 'Every deposit above 50, the balance receives 5% more profit' do
+    before do
       @account = Account.new
+    end
+
+    it 'Every deposit above 50, the balance receives 5% more profit' do
       @account.deposit(100.0)
       @account.deposit(50.0)
       expect(@account.balance).to eq(155.0)
+    end
+  end
+
+  context '#validations account' do
+    before do
+      @account = Account.new
+    end
+
+    it 'Withdraw only the amount you have available' do
+      @account.deposit(100.0)
+      @account.withdraw(150.0)
+      @account.deposit(300.0)
+      @account.withdraw(1000.0)
+      expect(@account.balance).to eq(0.0)
+    end
+
+    it "If withdraw equal zero, return message 'Value must be greater than zero'" do
+      @account.deposit(100.0)
+      expect(@account.withdraw(0.0)).to eq("Value must be greater than zero")
+    end
+
+    it 'If deposit equal zero, return message "Value must be greater than zero"' do
+      expect(@account.deposit(0.0)).to eq("Invalid amount")
     end
   end
 end
