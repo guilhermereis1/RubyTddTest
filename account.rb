02b13acc983756b4990316364100b0497c95d5b4
Guilherme R <guilhermejosereis27@gmail.com>
2022-03-24 10:53:41 -0400

    Created class Account and methods

---
+++ account.rb
@@ -0,0 +1,19 @@
+class Account
+  attr_reader :balance
+
+  def initialize(balance = 0.0)
+    @balance = balance
+  end
+
+  def show_balance
+    @balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+end
